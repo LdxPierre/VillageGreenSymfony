7eed1b6ae22edb9e27a4cabd2e25110b04e3af10
Pierre <pierre.ledoux@outlook.fr>
2023-04-26 09:14:27 +0200

    bootstrap theme

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+// bootstrap framework
+import * as bootstrap from 'bootstrap';
