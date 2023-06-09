21b3ec13da4c4e4e1a8d0742f2ddaf333e1f0bd8
Pierre <pierre.ledoux@outlook.fr>
2023-06-09 10:02:10 +0200

    cart into order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,16 +2,18 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Order;
+use App\Entity\Address;
 use App\Form\OrderType;
+use App\Form\AddressType;
+use App\Repository\OrderRepository;
 use App\Repository\AddressRepository;
 use App\Repository\CartItemRepository;
-use App\Repository\OrderRepository;
-use DateTime;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/order')]
 class OrderController extends AbstractController
@@ -19,20 +21,38 @@ class OrderController extends AbstractController
     #[Route('/new', name: 'app_order_new')]
     public function index(AddressRepository $addressRepository, Request $request, OrderRepository $orderRepository, CartItemRepository $cartItemRepository): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
         $user = $this->getUser();
         $order = new Order();
 
         // Add user addresses as choices
         $addresses = $addressRepository->findBy(['user' => $user]);
-        foreach ($addresses as $address) {
-            $options['address'][$address->__toString()] = $address->getId();
+        if ($addresses != null) {
+            foreach ($addresses as $address) {
+                $options['address'][$address->__toString()] = $address->getId();
+            }
+        } else {
+            $options['address']['Aucune adresse'] = null;
         }
 
-        // create form
+        // new address form
+        $address = new Address();
+        $addressForm = $this->createForm(AddressType::class, $address);
+        $addressForm->handleRequest($request);
+
+        if ($addressForm->isSubmitted() && $addressForm->isValid()) {
+            $address->setUser($this->getUser());
+            $addressRepository->save($address, true);
+
+            return $this->redirect($request->headers->get('referer'));
+        }
+
+        // create order form
         $form = $this->createForm(OrderType::class, $order, $options);
         $form->handleRequest($request);
 
-        // submit form
+        // submit order form
         if ($form->isSubmitted() && $form->isValid()) {
             $formData = $request->get('order');
             $shippingAddress = $addressRepository->findOneBy(['id' => $formData['shipping']]);
@@ -42,10 +62,10 @@ class OrderController extends AbstractController
                 ->setShipping($shippingAddress);
 
             // set billing address
-            if (isset($formData['billing'])) {
-                $order->setBilling($addressRepository->findOneBy(['id' => $formData['billing']]));
-            } else {
+            if (isset($formData['billingCheck'])) {
                 $order->setBilling($shippingAddress);
+            } else {
+                $order->setBilling($addressRepository->findOneBy(['id' => $formData['billing']]));
             }
 
             // flush order
@@ -61,6 +81,7 @@ class OrderController extends AbstractController
 
         return $this->render('order/new.html.twig', [
             'form' => $form->createView(),
+            'addressForm' => $addressForm->createView(),
         ]);
     }
 }
